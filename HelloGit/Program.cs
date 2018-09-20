3c08088accffe05a2326865022b193c7883ff918
ShIzBr <k.a.l.i.n@mail.ru>
2018-09-20 10:22:58 +0700

    added Console.WriteLine

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -10,6 +10,8 @@ namespace HelloGit
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello Git. I'm ShIzBr");
+            Console.ReadLine();
         }
     }
 }
