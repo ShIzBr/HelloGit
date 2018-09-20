9186d1257cf78fb404c51fe1095b3c9bfc08a065
ShIzBr <k.a.l.i.n@mail.ru>
2018-09-20 15:12:11 +0700

    add "_"

--- HelloGit/Goodbye.cs
+++ HelloGit/Goodbye.cs
@@ -12,7 +12,7 @@ namespace HelloGit
 
         public string Go(string message)
         {
-            return this.text + " " + message;
+            return this.text + "_" + message;
         }
     }
 
