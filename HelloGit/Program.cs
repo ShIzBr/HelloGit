22c1f2079cdb3b3442a2eb9448f3eb936a8553f1
ShIzBr <k.a.l.i.n@mail.ru>
2018-09-20 13:10:45 +0700

    Revert "добавил How are you"
    This reverts commit 300d703d48b0306979e489b24cba91225bbcf00f.

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -12,7 +12,6 @@ namespace HelloGit
         {
             Console.WriteLine("Hello Git. I'm ShIzBr");
             Console.WriteLine("Hello Git. I'm KrDPNsk");
-            Console.WriteLine("How are you?");
             Console.ReadLine();
         }
     }
